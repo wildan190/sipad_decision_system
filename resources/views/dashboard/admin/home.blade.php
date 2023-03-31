5111c409327a9e1afcd8a9977b5832c1aa496b36
wildanbelfiore <mikewildan@outlook.com>
2023-03-31 19:59:24 +0700

    Typo telah diperbaiki

--- resources/views/dashboard/admin/home.blade.php
+++ resources/views/dashboard/admin/home.blade.php
@@ -47,7 +47,7 @@
       <div class="row no-gutters align-items-center">
         <div class="col mr-2">
           <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Pending Assessement</div>
-          <div class="h5 mb-0 font-weight-bold text-gray-800">{{$assessment_pending}} person</div>
+          <div class="h5 mb-0 font-weight-bold text-gray-800">{{$assessment_pending}} Data</div>
         </div>
         <div class="col-auto">
           <i class="fas fa-times fa-2x text-gray-300"></i>
