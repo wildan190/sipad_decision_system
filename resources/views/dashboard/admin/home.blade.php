9c40d70cc9e168faa5ab7da4a6a9a0029326304d
wildanbelfiore <mikewildan@outlook.com>
2023-03-31 19:58:06 +0700

    Dashboard telah direvisi

--- resources/views/dashboard/admin/home.blade.php
+++ resources/views/dashboard/admin/home.blade.php
@@ -12,8 +12,8 @@
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Employe</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">{{$media}} Persons</div>
+              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Media</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">{{$media}} Data</div>
             </div>
             <div class="col-auto">
               <i class="fas fa-users fa-2x text-gray-300"></i>
