3b7aa6ed634f7ec925c7b348d88f42b5f791a0ed
wildanbelfiore <mikewildan@outlook.com>
2023-04-01 19:29:35 +0700

    Typo telah diperbaiki

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Simple Additive Weighting</title>
+        <title>Sinar Indah Padma</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,9 +81,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Simple Additive
+                    Sinar Indah
                     <br/>
-                    Weighting
+                    Padma
                 </div>
 
                 {{-- <div class="links">
