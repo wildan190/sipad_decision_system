7aef8c7709eaca0e141c41a635c9dbd19a178b63
wildanbelfiore <mikewildan@outlook.com>
2023-04-09 08:42:43 +0700

    footer telah diperbarui

--- resources/views/dashboard/partials/footer.blade.php
+++ resources/views/dashboard/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
       <div class="copyright text-center my-auto">
-        <span>Copyright &copy; Sinar Indah Padma 2023</span>
+        <span>Copyright &copy; <a href="https://www.instagram.com/sip_adv/" target="_BLANK">Sinar Indah Padma 2023</a></span>
       </div>
     </div>
   </footer>
